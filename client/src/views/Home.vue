a2f7d59ffe4c3b8d71db04ec0f95db894d0aa5ff
Colby Stone <colbyjstone@gmail.com>
2020-10-06 17:48:14 -0400

    Cleanup

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,23 +1,6 @@
 <template>
-  <div class="sample">
-    <h1>Example use of @carbon/vue</h1>
-    <cv-text-input
-      label="Who are you?"
-      v-model="yourName"
-      placeholder="your name"
-    />
-    <cv-button @click="onClick">Hello {{ yourName }}</cv-button>
-    <cv-modal :visible="visible" @modal-hidden="modalClosed">
-      <template slot="title">Welcome to @carbon/vue {{ yourName }}</template>
-      <template slot="content">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, seed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </p>
-      </template>
-    </cv-modal>
+  <div>
+    <h1>Setup</h1>
   </div>
 </template>
 
@@ -26,32 +9,9 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-      yourName: '',
-      visible: false,
     };
   },
   methods: {
-    onClick() {
-      this.visible = true;
-    },
-    modalClosed() {
-      this.visible = false;
-    },
   },
 };
 </script>
-
-<style>
-.sample {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  max-width: 600px;
-  margin: 5% auto;
-}
-
-.cv-text-input {
-  margin: 30px 0;
-}
-</style>
