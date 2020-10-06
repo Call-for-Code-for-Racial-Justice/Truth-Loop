5623872801c8385c3e8ed36309c7495ecd798ff4
Colby Stone <colbystone@colbys-air.lan>
2020-10-06 17:00:23 -0400

    Add carbon

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,11 @@
+import 'carbon-components/css/carbon-components.css';
+import CarbonComponentsVue from '@carbon/vue/src/index';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+Vue.use(CarbonComponentsVue);
 
 Vue.config.productionTip = false;
 
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="sample">
+    <h1>Example use of @carbon/vue</h1>
+    <cv-text-input
+      label="Who are you?"
+      v-model="yourName"
+      placeholder="your name"
+    />
+    <cv-button @click="onClick">Hello {{ yourName }}</cv-button>
+    <cv-modal :visible="visible" @modal-hidden="modalClosed">
+      <template slot="title">Welcome to @carbon/vue {{ yourName }}</template>
+      <template slot="content">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, seed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </template>
+    </cv-modal>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
+  name: 'HelloWorld',
+  data() {
+    return {
+      yourName: '',
+      visible: false,
+    };
+  },
+  methods: {
+    onClick() {
+      this.visible = true;
+    },
+    modalClosed() {
+      this.visible = false;
+    },
   },
 };
 </script>
+
+<style>
+.sample {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 600px;
+  margin: 5% auto;
+}
+
+.cv-text-input {
+  margin: 30px 0;
+}
+</style>
