690dc5fcc959735a25938d6cede48a7ba6f7ef5e
Pete Ihlenfeldt <penfeldt@us.ibm.com>
2020-10-08 14:25:27 -0500

    move advocacy group routes to module

--- policy-truth-backend/routes/advocacy_groups.js
+++ policy-truth-backend/routes/advocacy_groups.js
@@ -1,7 +1,24 @@
 const logger = require('../logger').logger;
 const advocacyGroupDB = require('../db/advocacy_groups');
+var express = require('express');
+var router = express.Router();
 
-const getAdvocacyGroups = (request, response) => {
+/**
+ * @api [get] /api/v1/advocacyGroups
+ * summary: Get a list of advocacy group objects
+ * tags:
+ *   - AdvocacyGroups
+ * responses:
+ *   200:
+ *     description: A list of advocacy group objects
+ *     content:
+ *       application/json:
+ *         schema:
+ *           type: array
+ *           items:
+ *             $ref: "#/components/schemas/AdvocacyGroup"
+ */
+router.get('/', (request, response) => {
   advocacyGroupDB.getAdvocacyGroups((error, results) => {
       if (error) {
           logger.error("failed to retrieve advocacy groups: %s", error)
@@ -12,9 +29,30 @@ const getAdvocacyGroups = (request, response) => {
           response.status(200).json(results.rows)
       }
   })
-}
+})
 
-const getAdvocacyGroupById = (request, response) => {
+/**
+ * @api [get] /api/v1/advocacyGroups/{id}
+ * summary: Get an advocacy group object
+ * tags:
+ *   - AdvocacyGroups
+ * parameters:
+ *   - in: path
+ *     name: id
+ *     schema:
+ *       type: integer
+ *     description: The advocacy group ID
+ * responses:
+ *   200:
+ *     description: AdvocacyGroup object found
+ *     content:
+ *       application/json:
+ *         schema:
+ *           $ref: "#/components/schemas/AdvocacyGroup"
+ *   404:
+ *     description: No Advocacy Group object exists for that id
+ */
+router.get('/:id', (request, response) => {
   const id = parseInt(request.params.id)
   advocacyGroupDB.getAdvocacyGroupById(id, (error, results) => {
       if (error) {
@@ -32,9 +70,26 @@ const getAdvocacyGroupById = (request, response) => {
           }
       }
   })
-}
+})
 
-const createAdvocacyGroup = (request, response) => {
+/**
+ * @api [post] /api/v1/advocacyGroups
+ * summary: Create an advocacy group object
+ * tags:
+ *   - AdvocacyGroups
+ * parameters:
+ *   - in: body
+ *     schema:
+ *       $ref: "#/components/schemas/AdvocacyGroup"
+ * responses:
+ *   201:
+ *     description: ID of Advocacy Group object added
+ *     content:
+ *       application/json:
+ *         schema:
+ *           $ref: "#/components/schemas/IdOfCreatedObject"
+ */
+router.post('/', (request, response) => {
     advocacyGroupDB.createAdvocacyGroup(request.body, (error, results) => {
         if (error) {
             logger.error("fail to create advocacy group: %s", error)
@@ -45,9 +100,33 @@ const createAdvocacyGroup = (request, response) => {
             response.status(201).json(results.rows[0])
         }
     })
-}
+})
 
-const updateAdvocacyGroup = (request, response) => {
+/**
+ * @api [put] /api/v1/advocacyGroups
+ * summary: Update an advocacy group object
+ * tags:
+ *   - AdvocacyGroups
+ * parameters:
+ *   - in: path
+ *     name: id
+ *     schema:
+ *       type: integer
+ *     description: The advocacy group ID
+ *   - in: body
+ *     schema:
+ *       $ref: "#/components/schemas/AdvocacyGroup"
+ * responses:
+ *   200:
+ *     description: confirmation of success
+ *     content:
+ *       application/json:
+ *         schema:
+ *           $ref: "#/components/schemas/ConfirmationOfSuccess"
+ *   404:
+ *     description: No Advocacy Group object exists for that id
+ */
+router.put('/:id', (request, response) => {
   const id = parseInt(request.params.id)
   advocacyGroupDB.updateAdvocacyGroup(id, request.body, (error, results) => {
       if (error) {
@@ -67,9 +146,30 @@ const updateAdvocacyGroup = (request, response) => {
           }
       }
   })
-}
+})
 
-const deleteAdvocacyGroup = (request, response) => {
+/**
+ * @api [delete] /api/v1/advocacyGroups/{id}
+ * summary: Delete an advocacy group object
+ * tags:
+ *   - AdvocacyGroups
+ * parameters:
+ *   - in: path
+ *     name: id
+ *     schema:
+ *       type: integer
+ *     description: The advocacy group ID
+ * responses:
+ *   200:
+ *     description: confirmation of success
+ *     content:
+ *       application/json:
+ *         schema:
+ *           $ref: "#/components/schemas/ConfirmationOfSuccess"
+ *   404:
+ *     description: No Advocacy Group object exists for that id
+ */
+router.delete('/:id', (request, response) => {
   const id = parseInt(request.params.id)
   advocacyGroupDB.deleteAdvocacyGroup(id, (error, results) => {
       if (error) {
@@ -89,12 +189,6 @@ const deleteAdvocacyGroup = (request, response) => {
           }
       }
   })
-}
+})
 
-module.exports = {
-  getAdvocacyGroups,
-  getAdvocacyGroupById,
-  createAdvocacyGroup,
-  updateAdvocacyGroup,
-  deleteAdvocacyGroup,
-}
+module.exports = router
