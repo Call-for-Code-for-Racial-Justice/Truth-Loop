c104f5fb9d57b318ae51f22ce734d8ec4861726d
Pete Ihlenfeldt <penfeldt@us.ibm.com>
2020-10-08 12:25:10 -0500

    added margin stars back to schema.js and corrected date-time datatype

--- policy-truth-backend/routes/schema.js
+++ policy-truth-backend/routes/schema.js
@@ -1,74 +1,74 @@
 //////////////////////////////////////////// OpenAPI Schema Objects ////////////////////////////////////////////
 
 /**
- @schema IdOfCreatedObject
- description: the returned ID of a successfully created object
- type: object
- properties:
-   id:
-     type: integer
+ * @schema IdOfCreatedObject
+ * description: the returned ID of a successfully created object
+ * type: object
+ * properties:
+ *   id:
+ *     type: integer
  */
 
 /**
- @schema ConfirmationOfSuccess
- description: confirmation that the last operation was successful
- type: object
- properties:
-   ok:
-     type: boolean
+ * @schema ConfirmationOfSuccess
+ * description: confirmation that the last operation was successful
+ * type: object
+ * properties:
+ *   ok:
+ *     type: boolean
  */
 
 /**
-  @schema Category
-  description: A Category defines....
-  type: object
-  properties:
-    id:
-      type: integer
-      readOnly: true
-    name:
-      type: string
-    description:
-      type: string
-    created:
-      type: timestamp
-      readOnly: true
-      format: date-time
-      description: timestamp object first created
-    updated:
-      type: timestamp
-      readOnly: true
-      format: date-time
-      description: timestamp object last updated
-  required:
-    - name
+ * @schema Category
+ * description: A Category defines....
+ * type: object
+ * properties:
+ *   id:
+ *     type: integer
+ *     readOnly: true
+ *   name:
+ *     type: string
+ *   description:
+ *     type: string
+ *   created:
+ *     type: date-time
+ *     readOnly: true
+ *     format: date-time
+ *     description: date-time object first created
+ *   updated:
+ *     type: date-time
+ *     readOnly: true
+ *     format: date-time
+ *     description: date-time object last updated
+ * required:
+ *   - name
  */
 
 /**
-  @schema GeospatialDefinition
-  description: A Geospatial Definition defines....
-  type: object
-  properties:
-    id:
-      type: integer
-      readOnly: true
-    name:
-      type: string
-    short_name_ui:
-      type: string
-    description:
-      type: string
-    created:
-      type: timestamp
-      readOnly: true
-      format: date-time
-      description: timestamp object first created
-    updated:
-      type: timestamp
-      readOnly: true
-      format: date-time
-      description: timestamp object last updated
-  required:
-    - name
-    - short_name_ui
+ * @schema GeospatialDefinition
+ * description: A Geospatial Definition defines....
+ * type: object
+ * properties:
+ *   id:
+ *     type: integer
+ *     readOnly: true
+ *   name:
+ *     type: string
+ *   short_name_ui:
+ *     type: string
+ *   description:
+ *     type: string
+ *   created:
+ *     type: date-time
+ *     readOnly: true
+ *     format: date-time
+ *     description: date-time object first created
+ *   updated:
+ *     type: date-time
+ *     readOnly: true
+ *     format: date-time
+ *     description: date-time object last updated
+ * required:
+ *   - name
+ *   - short_name_ui
  */
