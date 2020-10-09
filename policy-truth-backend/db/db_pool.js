f2f3cf465dde581ceb1728ab00af93d448d136e8
Mark Sturdevant <mark.sturdevant@ibm.com>
2020-10-09 08:07:56 -0700

    Optionally, allow a SSL connection with required cert
    This works with EDB (Enterprise Postgres) on IBM Cloud.
    Ignored if not configured.

--- policy-truth-backend/db/db_pool.js
+++ policy-truth-backend/db/db_pool.js
@@ -1,18 +1,36 @@
 const Pool = require('pg').Pool
+const fs = require('fs');
 
 var dbHost = process.env.DB_HOST || 'localhost';
 var dbUserName = process.env.DB_USERNAME || 'NO_SET';
 var dbPassword = process.env.DB_PASSWORD || 'NO_SET';
 var dbPort = process.env.DB_PORT || 5432;
 var dbDatabaseName = process.env.DB_DATABASE_NAME || 'policydb';
+var dbCertFile = process.env.DB_CERTFILE;
 
-const pool = new Pool({
-    host: dbHost,
-    port: dbPort,
-    database: dbDatabaseName,
-    user: dbUserName,
-    password:  dbPassword,
-})
+
+let ssl = false;
+if (dbCertFile) {
+  ssl = {
+    rejectUnauthorized: false,
+    ca: fs.readFileSync(process.env.DB_CERTFILE).toString()
+  }
+}
+
+
+const config = {
+  host: dbHost,
+  port: dbPort,
+  database: dbDatabaseName,
+  user: dbUserName,
+  password: dbPassword,
+};
+
+if (ssl) {
+  config.ssl = ssl;
+}
+
+const pool = new Pool(config);
 
 module.exports = {
     pool
