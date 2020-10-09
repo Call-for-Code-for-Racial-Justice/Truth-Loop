9f03f58e11662741e1cbf0d63a4ea2fa3f019638
TANUSHREE PAUL <tanpaul1@in.ibm.com>
2020-10-09 12:13:20 +0530

    Added Vue Comp Policy & PolicyNotice

---
+++ client/src/components/Policy/Policy.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Policy Item {{ item }}</div>
+</template>
+<script>
+export default {
+  name: 'Policy',
+  props: ['item'],
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ client/src/components/PolicyNotice/PolicyNotice.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <p>Policy Privacy Notice</p>
+      <CvButton>Button</CvButton>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'PolicyNotice',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
