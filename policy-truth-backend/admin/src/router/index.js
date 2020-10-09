3ffdb99e4339f85f1254b89a2511bc824d8cd823
Mark Sturdevant <mark.sturdevant@ibm.com>
2020-10-09 09:01:26 -0700

    Update Admin UI fetch URL, remove junk, add to server.
    * Updated URL (still local for dev)
    * Removing irrelevant code.
    * server.js will serve up /dist
    * (tested w/ no /dist, but a static /dist it coming next)

--- policy-truth-backend/admin/src/router/index.js
+++ policy-truth-backend/admin/src/router/index.js
@@ -10,11 +10,6 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue'),
   },
   {
-    path: '/tickets',
-    name: 'Tickets',
-    component: () => import(/* webpackChunkName: "tickets" */ '../views/AllTickets.vue'),
-  },
-  {
     path: '/artifactform',
     name: 'Artifact Form',
     component: () => import(/* webpackChunkName: "artifactform" */ '../views/ArtifactForm.vue'),
