1687049bba7a3a9f10bf5a21a57058fb6a2b0a3c
Mark Sturdevant <mark.sturdevant@ibm.com>
2020-10-09 08:29:35 -0700

    Admin UI scaffolding and first draft add-artifact. To be served by server.
    * Consistent with initial client (Vue.js, Carbon, etc...) so far.
    * Will build and serve in server with separate commit.
    * W-I-P placeholders and using hard-coded localhost:5000 server for now.

---
+++ policy-truth-backend/admin/src/App.vue
@@ -0,0 +1,107 @@
+<template>
+  <div id="app">
+    <cv-header aria-label="Carbon header">
+      <cv-header-menu-button aria-label="Header menu" aria-controls="side-nav" />
+      <cv-skip-to-content href="#main-content">
+        Skip to content
+      </cv-skip-to-content>
+      <cv-header-name href="javascript:void(0)">
+        TRUTH Admin UI
+      </cv-header-name>
+      <cv-header-nav aria-label="Carbon nav">
+        <cv-header-menu-item href="#/artifactform">
+          Add Legislative Artifact
+        </cv-header-menu-item>
+        <cv-header-menu-item href="javascript:void(0)">
+          Add other thing
+        </cv-header-menu-item>
+        <cv-header-menu-item href="javascript:void(0)">
+          And more
+        </cv-header-menu-item>
+        <cv-header-menu aria-label="Mores" title="Mores">
+          <cv-header-menu-item href="javascript:void(0)">
+            more
+          </cv-header-menu-item>
+          <cv-header-menu-item href="javascript:void(0)">
+            mores
+          </cv-header-menu-item>
+          <cv-header-menu-item href="javascript:void(0)">
+            smores
+          </cv-header-menu-item>
+        </cv-header-menu>
+      </cv-header-nav>
+      <template v-slot:left-panels>
+
+        <cv-side-nav id="side-nav" rail>
+          <cv-side-nav-items>
+            <cv-header-side-nav-items divider>
+              <cv-header-menu-item href="#/artifactform">
+                Add Legislative Artifact
+              </cv-header-menu-item>
+              <cv-header-menu-item href="javascript:void(0)">
+                Add other thing
+              </cv-header-menu-item>
+              <cv-header-menu-item href="javascript:void(0)">
+                And more
+              </cv-header-menu-item>
+              <cv-header-menu aria-label="Link 4" title="Link 4" :hover-toggle="false">
+                <cv-header-menu-item href="javascript:void(0)">
+                  Submenu Link 1
+                </cv-header-menu-item>
+                <cv-header-menu-item href="javascript:void(0)">
+                  Submenu Link 2
+                </cv-header-menu-item>
+                <cv-header-menu-item href="javascript:void(0)">
+                  Submenu Link 3
+                </cv-header-menu-item>
+              </cv-header-menu>
+            </cv-header-side-nav-items>
+
+            <cv-side-nav-menu title="Legislative Artifacts">
+              <cv-side-nav-menu-item href="#/artifactform" active>
+                Add
+              </cv-side-nav-menu-item>
+              <cv-side-nav-menu-item href="javascript:void(0)">
+                List
+              </cv-side-nav-menu-item>
+              <cv-side-nav-menu-item href="javascript:void(0)">
+                Count
+              </cv-side-nav-menu-item>
+            </cv-side-nav-menu>
+            <cv-side-nav-menu title="L1 menu">
+              <cv-side-nav-menu-item href="javascript:void(0)">
+                L2 menu item
+              </cv-side-nav-menu-item>
+              <cv-side-nav-menu-item href="javascript:void(0)" aria-current="page">
+                L2 menu item
+              </cv-side-nav-menu-item>
+              <cv-side-nav-menu-item href="javascript:void(0)">
+                L2 menu item
+              </cv-side-nav-menu-item>
+            </cv-side-nav-menu>
+            <cv-side-nav-link href="javascript:void(0)">
+              L1 link
+            </cv-side-nav-link>
+            <cv-side-nav-link href="http://localhost:5000/api-docs/">
+              API Docs
+            </cv-side-nav-link>
+
+          </cv-side-nav-items>
+        </cv-side-nav>
+      </template>
+    </cv-header>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+
+  body {
+  }
+
+  #app {
+    justify-content: center;
+    align-items: center;
+    margin: 5rem 0 0 5rem;
+  }
+</style>
