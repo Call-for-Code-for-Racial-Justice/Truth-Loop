c5d37316a3f97ab0f010d9a488641b564a40c65d
Mark Sturdevant <mark.sturdevant@ibm.com>
2020-10-11 03:04:46 -0700

    Add categories/publications form and tables in Admin UI
    * Table gets items and has delete and edit actions
    * Form for adding items
    * Minimal functionality. These work, but are not complete.

--- policy-truth-backend/admin/src/router/index.js
+++ policy-truth-backend/admin/src/router/index.js
@@ -10,14 +10,34 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue'),
   },
   {
-    path: '/artifactform',
-    name: 'Artifact Form',
-    component: () => import(/* webpackChunkName: "artifactform" */ '../views/ArtifactForm.vue'),
+    path: '/artifactsform',
+    name: 'Add Artifacts',
+    component: () => import(/* webpackChunkName: "artifactsform" */ '../views/ArtifactForm.vue'),
   },
   {
-    path: '/artifacttable',
-    name: 'Artifact table',
-    component: () => import(/* webpackChunkName: "artifactform" */ '../views/ArtifactTable.vue'),
+    path: '/artifacts',
+    name: 'Artifacts',
+    component: () => import(/* webpackChunkName: "artifacts" */ '../views/ArtifactTable.vue'),
+  },
+  {
+    path: '/categories/form',
+    name: 'Add Categories',
+    component: () => import(/* webpackChunkName: "categoriesadd" */ '../views/CategoriesForm.vue'),
+  },
+  {
+    path: '/categories',
+    name: 'Categories',
+    component: () => import(/* webpackChunkName: "categories" */ '../views/Categories.vue'),
+  },
+  {
+    path: '/publications/form',
+    name: 'Add Publications',
+    component: () => import(/* webpackChunkName: "publicationsform" */ '../views/PublicationsForm.vue'),
+  },
+  {
+    path: '/publications',
+    name: 'Publications',
+    component: () => import(/* webpackChunkName: "publications" */ '../views/Publications.vue'),
   },
 ];
 
