1b51c34e3b43c84b59d398048356c70b7ba9852a
Mark Sturdevant <mark.sturdevant@ibm.com>
2020-10-11 03:04:46 -0700

    Add categories/publications form and tables in Admin UI
    * Table gets items and has delete and edit actions
    * Form for adding items
    * Minimal functionality. These work, but are not complete.

--- policy-truth-backend/admin/src/App.vue
+++ policy-truth-backend/admin/src/App.vue
@@ -6,87 +6,37 @@
         Skip to content
       </cv-skip-to-content>
       <cv-header-name href="javascript:void(0)">
-        TRUTH Admin UI
+        Policy Truth Admin UI
       </cv-header-name>
-      <cv-header-nav aria-label="Carbon nav">
-        <cv-header-menu-item href="#/artifactform">
-          Add Legislative Artifact
-        </cv-header-menu-item>
-        <cv-header-menu-item href="javascript:void(0)">
-          Add other thing
-        </cv-header-menu-item>
-        <cv-header-menu-item href="javascript:void(0)">
-          And more
-        </cv-header-menu-item>
-        <cv-header-menu aria-label="Mores" title="Mores">
-          <cv-header-menu-item href="javascript:void(0)">
-            more
-          </cv-header-menu-item>
-          <cv-header-menu-item href="javascript:void(0)">
-            mores
-          </cv-header-menu-item>
-          <cv-header-menu-item href="javascript:void(0)">
-            smores
-          </cv-header-menu-item>
-        </cv-header-menu>
-      </cv-header-nav>
       <template v-slot:left-panels>
-
         <cv-side-nav id="side-nav" rail>
-          <cv-side-nav-items>
-            <cv-header-side-nav-items divider>
-              <cv-header-menu-item href="#/artifactform">
-                Add Legislative Artifact
-              </cv-header-menu-item>
-              <cv-header-menu-item href="javascript:void(0)">
-                Add other thing
-              </cv-header-menu-item>
-              <cv-header-menu-item href="javascript:void(0)">
-                And more
-              </cv-header-menu-item>
-              <cv-header-menu aria-label="Link 4" title="Link 4" :hover-toggle="false">
-                <cv-header-menu-item href="javascript:void(0)">
-                  Submenu Link 1
-                </cv-header-menu-item>
-                <cv-header-menu-item href="javascript:void(0)">
-                  Submenu Link 2
-                </cv-header-menu-item>
-                <cv-header-menu-item href="javascript:void(0)">
-                  Submenu Link 3
-                </cv-header-menu-item>
-              </cv-header-menu>
-            </cv-header-side-nav-items>
-
-            <cv-side-nav-menu title="Legislative Artifacts">
-              <cv-side-nav-menu-item href="#/artifactform" active>
-                Add Artifact
-              </cv-side-nav-menu-item>
-              <cv-side-nav-menu-item href="#/artifacttable">
-                List/Edit Artifacts
-              </cv-side-nav-menu-item>
-              <cv-side-nav-menu-item href="javascript:void(0)">
-                Count
-              </cv-side-nav-menu-item>
-            </cv-side-nav-menu>
-            <cv-side-nav-menu title="L1 menu">
-              <cv-side-nav-menu-item href="javascript:void(0)">
-                L2 menu item
-              </cv-side-nav-menu-item>
-              <cv-side-nav-menu-item href="javascript:void(0)" aria-current="page">
-                L2 menu item
-              </cv-side-nav-menu-item>
-              <cv-side-nav-menu-item href="javascript:void(0)">
-                L2 menu item
-              </cv-side-nav-menu-item>
-            </cv-side-nav-menu>
-            <cv-side-nav-link href="javascript:void(0)">
-              L1 link
-            </cv-side-nav-link>
-            <cv-side-nav-link href="/api-docs/">
-              API Docs
-            </cv-side-nav-link>
-
-          </cv-side-nav-items>
+          <cv-side-nav-link href="/api-docs/">
+            API Docs
+          </cv-side-nav-link>
+          <cv-side-nav-menu title="Legislative Artifacts">
+            <cv-side-nav-menu-item href="#/artifactsform" active>
+              Add Artifacts
+            </cv-side-nav-menu-item>
+            <cv-side-nav-menu-item href="#/artifacts">
+              List/Edit/Delete Artifacts
+            </cv-side-nav-menu-item>
+          </cv-side-nav-menu>
+          <cv-side-nav-menu title="Categories">
+            <cv-side-nav-menu-item href="#/categories/form" active>
+              Add Categories
+            </cv-side-nav-menu-item>
+            <cv-side-nav-menu-item href="#/categories">
+              List/Edit/Delete Categories
+            </cv-side-nav-menu-item>
+          </cv-side-nav-menu>
+          <cv-side-nav-menu title="Publications">
+            <cv-side-nav-menu-item href="#/publications/form" active>
+              Add Publications
+            </cv-side-nav-menu-item>
+            <cv-side-nav-menu-item href="#/publications">
+              List/Edit/Delete Publications
+            </cv-side-nav-menu-item>
+          </cv-side-nav-menu>
         </cv-side-nav>
       </template>
     </cv-header>
@@ -95,9 +45,6 @@
 </template>
 
 <style>
-
-  body {
-  }
 
   #app {
     justify-content: center;
---
+++ policy-truth-backend/admin/src/views/Publications.vue
@@ -0,0 +1,267 @@
+<template>
+  <div data-app id="publications">
+    <v-card>
+      <v-card-title>
+        All Publications
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+
+      <v-data-table
+        :headers="headers"
+        :items="this.items"
+        item-key="id"
+        :sort-by="['title']"
+        :sort-desc="[false]"
+        :search="search"
+        dense
+        group-desc
+        show-group-by
+        multi-sort
+        class="elevation-24">
+        <template v-slot:top>
+          <v-spacer></v-spacer>
+          <v-dialog
+            v-model="dialogEdit"
+            max-width="500px"
+          >
+            <v-card>
+              <v-card-title>
+                <span class="headline">Edit Item</span>
+              </v-card-title>
+
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-text-field
+                        v-model="editedItem.title"
+                        label="Title"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-text-field
+                        v-model="editedItem.description"
+                        label="Description"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                    </v-col>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="cancel"
+                >
+                  Cancel
+                </v-btn>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="save"
+                >
+                  Save
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-card>
+              <v-card-title class="headline">
+                Are you sure you want to delete this item?
+              </v-card-title>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </template>
+
+        <template v-slot:item.created="{ item }">
+          {{ new Date(item.created).toLocaleDateString() }}
+        </template>
+        <template v-slot:item.updated="{ item }">
+          {{ new Date(item.updated).toLocaleDateString() }}
+        </template>
+        <template v-slot:item.actions="{ item }">
+          <v-icon
+            small
+            @click="editItem(item)"
+          >
+            mdi-pencil
+          </v-icon>
+          <v-icon
+            small
+            @click="deleteDialog(item)"
+          >
+            mdi-delete
+          </v-icon>
+        </template>
+
+      </v-data-table>
+    </v-card>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'publications',
+  data() {
+    return {
+      items: [],
+      dialogEdit: false,
+      dialogDelete: false,
+      editedIndex: -1,
+      editedItem: {},
+      cachedItem: {},
+      search: '',
+      headers: [
+        {
+          text: 'ID', value: 'id', filterable: true, groupable: false,
+        },
+        {
+          text: 'Title', value: 'title', filterable: true, groupable: false,
+        },
+        {
+          text: 'Description', value: 'description', filterable: true, groupable: false,
+        },
+        {
+          text: 'Date Created', value: 'created', filterable: false, groupable: false,
+        },
+        {
+          text: 'Date Updated', value: 'updated', filterable: false, groupable: false,
+        },
+        {
+          text: 'Actions', value: 'actions', filterable: false, groupable: false,
+        },
+      ],
+    };
+  },
+  mounted() {
+    this.getItems();
+  },
+  methods: {
+    isUrl(link) {
+      return !!link && (link.length > 7) && link.startsWith('http');
+    },
+    async getItems() {
+      try {
+        const response = await fetch('/api/v1/publications');
+        this.items = await response.json();
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    editMode(item) {
+      this.cachedItem = { ...item };
+    },
+    cancelEdit(item) {
+      Object.assign(item, this.cachedItem);
+      this.items.splice(this.editedIndex, 1, this.cachedItem);
+      this.dialogEdit = false;
+    },
+
+    editItem(item) {
+      this.editMode(item);
+      this.editedIndex = this.items.indexOf(item);
+      this.editedItem = item;
+      this.dialogEdit = true;
+    },
+
+    async deleteItem(id) {
+      try {
+        console.log('deleteItem...');
+        const response = await fetch(`/api/v1/publications/${id}`, {
+          method: 'DELETE',
+        });
+        console.log('RESPONSE:', response);
+        const data = await response.json();
+        console.log('DATA:', data);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    deleteDialog(item) {
+      this.editedIndex = this.items.indexOf(item);
+      this.dialogDelete = true;
+    },
+    deleteItemConfirm() {
+      const item = this.items[this.editedIndex];
+      this.deleteItem(item.id);
+      this.items.splice(this.editedIndex, 1);
+      this.closeDelete();
+    },
+    closeDelete() {
+      this.dialogDelete = false;
+      this.$nextTick(() => {
+        this.editedIndex = -1;
+      });
+    },
+    save() {
+      this.updateItem(this.items[this.editedIndex]);
+      this.dialogEdit = false;
+    },
+    cancel(item) {
+      this.cancelEdit(item);
+    },
+    open() {
+      console.log('open()');
+    },
+    close() {
+      console.log('Dialog closed');
+      this.dialogEdit = false;
+      console.log('close()');
+    },
+    async updateItem(item) {
+      try {
+        await fetch(
+          `/api/v1/publications/${item.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(item),
+            headers: { 'Content-type': 'application/json; charset=UTF-8' },
+          },
+        );
+        this.items.splice(this.editedIndex, 1, item);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
+</script>
