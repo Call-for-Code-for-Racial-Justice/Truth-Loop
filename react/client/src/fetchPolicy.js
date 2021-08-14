6b66994669b606b8fc828ddfd9850a1deca50369
ed.snodgrass <snodgrass.ed@gmail.com>
2021-08-14 09:43:12 -0500

    Moving the mock data local to react client so fetchPolicy can find it.
    Signed-off-by: Ed Snodgrass <snodgrass.ed@gmail.com>

--- react/client/src/fetchPolicy.js
+++ react/client/src/fetchPolicy.js
@@ -1,4 +1,4 @@
-import policy from '../../../client/mockdata/CURRENT_FULL_RETRIEVAL_OF_ARTIFACT_1'
+import policy from './mockdata/CURRENT_FULL_RETRIEVAL_OF_ARTIFACT_1'
 
 export default async function fetchPolicy(policyId) {
   if (process.env.VUE_APP_MOCK_DATA) {
