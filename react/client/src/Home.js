cbc54f1b53219bca9dfed0cb83bd1e9b6db9710d
Ed Snodgrass <snodgrass.ed@gmail.com>
2021-08-27 11:24:06 -0500

    Added logic in Home view to show correct components. Added tests too. npm installed redux-mock-store to assist with testing.
    Signed-off-by: Ed Snodgrass <snodgrass.ed@gmail.com>

--- react/client/src/Home.js
+++ react/client/src/Home.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import PolicyNotice from './PolicyNotice'
+import {useSelector} from 'react-redux'
+import PolicyTable from './PolicyTable'
 
 const Home = () => {
+
+  const {privacyAccepted, privacyCancelled} = useSelector(({privacy}) => privacy)
+  const renderPrivacyCancelContent = () => {
+    return (
+      <div data-testid={'pleaseAcceptPrivacyStatement'} className="privacy-cancel-content">
+        <h5>Please accept the Privacy Statement.</h5>
+        <p>Sorry! content is not available without accepting the privacy policy.
+          <br/>Please refresh this page and accept the privacy statement.</p>
+      </div>
+    )
+  }
+
   return (
-    <h1>Home</h1>
+    <div className="home-content">
+      <div className="bx--grid bx--grid--full-width bx--grid--no-gutter ">
+        <div className="bx--row">
+          <div className="bx--col">
+            {!privacyAccepted && <PolicyNotice/>}
+            {privacyAccepted && <PolicyTable/>}
+            {privacyCancelled && renderPrivacyCancelContent()}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
