9852da6c94afb16d294c14148695a03ed6fd7235
Ed Snodgrass <snodgrass.ed@gmail.com>
2021-08-27 13:54:13 -0500

    Added carbon styling to react client to get the basic App and Home styles set up.
    Signed-off-by: Ed Snodgrass <snodgrass.ed@gmail.com>

--- react/client/src/App.js
+++ react/client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {BrowserRouter, Route, Switch, Link} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import './App.scss'
 import Home from './Home'
 import Policy from './Policy'
 import Record from './Record'
@@ -7,24 +8,15 @@ import Record from './Record'
 function App() {
   return (
     <BrowserRouter>
-      <h1>Welcome to TruthLoop!</h1>
-      <ul>
-        <li>
-          <Link name="home-link" to="/">Home</Link>
-        </li>
-        <li>
-          <Link name="policy-link" to="/policy/1234">Policy</Link>
-        </li>
-        <li>
-          <Link name="record-link" to="/policy/1234/record">Record</Link>
-        </li>
-      </ul>
-      <hr />
-      <Switch>
-          <Route name="home-route" exact path={'/'} component={Home} />
-          <Route name="policy-route" exact path={'/policy/:policyId'} component={Policy} />
-          <Route name="record-route" exact path={'/policy/:policyId/record'} component={Record} />
-      </Switch>
+      <div id={'app'}>
+        <div id={'main-content'}>
+          <Switch>
+            <Route name="home-route" exact path={'/'} component={Home} />
+            <Route name="policy-route" exact path={'/policy/:policyId'} component={Policy} />
+            <Route name="record-route" exact path={'/policy/:policyId/record'} component={Record} />
+          </Switch>
+        </div>
+      </div>
     </BrowserRouter>
   )
 }
--- react/client/src/Home.js
+++ react/client/src/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import PolicyNotice from './PolicyNotice'
 import {useSelector} from 'react-redux'
+import './Home.scss'
+import PolicyNotice from './PolicyNotice'
 import PolicyTable from './PolicyTable'
 
 const Home = () => {
