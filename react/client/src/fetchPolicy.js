6a1a348d752f28c22fe34dda65a0e346ff2a4dec
Ed Snodgrass <snodgrass.ed@gmail.com>
2021-08-29 23:16:24 -0500

    #145 switching env variables to REACT_APP
    Signed-off-by: Ed Snodgrass <snodgrass.ed@gmail.com>

--- react/client/src/fetchPolicy.js
+++ react/client/src/fetchPolicy.js
@@ -1,11 +1,11 @@
 import policy from './mockdata/CURRENT_FULL_RETRIEVAL_OF_ARTIFACT_1'
 
 export default async function fetchPolicy(policyId) {
-  if (process.env.VUE_APP_MOCK_DATA) {
+  if (process.env.REACT_APP_MOCK_DATA) {
     console.log('Using MOCK DATA for policy store')
-    return policy
+    return { ...policy, id: policyId }
   } else {
-    const apiUrl = process.env.VUE_APP_SERVER_URL || ''
+    const apiUrl = process.env.REACT_APP_SERVER_URL || ''
     const policyFullDetailResponse = await fetch(`${apiUrl}/api/v1/legislativeArtifacts/fullDetail/${policyId}`)
     return policyFullDetailResponse.json()
   }
