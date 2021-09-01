606b5b0b1e4d70b76ba59a7f3f6eb0056b0fa30b
Ed Snodgrass <snodgrass.ed@gmail.com>
2021-08-31 21:32:59 -0500

    #145 PolicyRoute fetch of policy and handling statuses
    Signed-off-by: Ed Snodgrass <snodgrass.ed@gmail.com>

---
+++ react/client/src/policyDetail/PolicyDetail.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const PolicyDetail = () => {
+  return <div>Policy Detail</div>
+}
+PolicyDetail.propTypes = {
+  policy: PropTypes.object.isRequired,
+}
+export default PolicyDetail
--- react/client/src/policyRoute/PolicyRoute.js
+++ react/client/src/policyRoute/PolicyRoute.js
@@ -1,13 +1,28 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, {useEffect} from 'react'
+import {useParams} from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
+import {fetchCurrentPolicy} from '../store/policy.duck'
+import PolicyDetail from '../policyDetail/PolicyDetail'
 
 const PolicyRoute = () => {
-  const { policyId } = useParams()
+  const {policyId} = useParams()
+  const dispatch = useDispatch()
+  const {currentPolicy, status} = useSelector(({policy}) => policy)
 
-  if (policyId) {
-    return <h2>Policy Detail for {policyId}</h2>
+  useEffect(() => {
+    if (status === 'idle' && (!currentPolicy || currentPolicy.id !== policyId)) {
+      dispatch(fetchCurrentPolicy(policyId))
+    }
+  }, [dispatch, status, policyId, currentPolicy])
+
+  if (currentPolicy && status === 'idle') {
+    return <h2 data-testid={'policyDetail'}><PolicyDetail policy={currentPolicy}/></h2>
+  } else if (status === 'pending') {
+    return <h2 data-testid={'loadingPolicy'}>Loading...</h2>
+  } else if (status === 'error') {
+    return <h2 data-testid={'cannotLoadPolicy'}>Cannot load policy</h2>
   }
-  return <h2>No policy found</h2>
+  return <h2 data-testid={'emptyPolicy'}>No policy found</h2>
 }
 
 export default PolicyRoute
