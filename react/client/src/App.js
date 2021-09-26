c61a4ab6b655b1c072c4685ebd8ca295041b6b15
felizj17 <felizj17@gmail.com>
2021-09-26 19:15:36 -0400

    filter-menu added to the apptopbar
    Signed-off-by: felizj17 <felizj17@gmail.com>

--- react/client/src/App.js
+++ react/client/src/App.js
@@ -4,12 +4,13 @@ import './App.scss'
 import HomeRoute from './homeRoute/HomeRoute'
 import PolicyRoute from './policyRoute/PolicyRoute'
 import RecordRoute from './recordRoute/RecordRoute'
-
+import AppTopBar from './appTopBar/AppTopBar'
 function App() {
   return (
     <BrowserRouter>
       <div id={'app'}>
         <div id={'main-content'} data-testid={'mainContent'}>
+        <AppTopBar />
           <Switch>
             <Route name="home-route" exact path={'/'} component={HomeRoute} />
             <Route name="policy-route" exact path={'/policy/:policyId'} component={PolicyRoute} />
