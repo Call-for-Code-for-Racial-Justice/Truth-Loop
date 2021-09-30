f89f576a174d0bc2004632b3c25b4fb007be2131
Ed Snodgrass <snodgrass.ed@gmail.com>
2021-09-30 08:58:25 -0500

    #157 React component that actually records video and allows it to be played in the browser.
    Signed-off-by: Ed Snodgrass <snodgrass.ed@gmail.com>

--- react/client/src/recordRoute/RecordRoute.js
+++ react/client/src/recordRoute/RecordRoute.js
@@ -1,41 +1,88 @@
-import React, { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, {useCallback, useRef, useState} from 'react'
+import {useParams} from 'react-router-dom'
 import {Column, Grid, Row} from 'carbon-components-react'
 import StopFilledAlt32 from '@carbon/icons-react/lib/stop--filled--alt/32'
 import RecordingFilledAlt32 from '@carbon/icons-react/lib/recording--filled--alt/32'
+import Webcam from 'react-webcam'
+import RecordRTC from 'recordrtc'
 
 import './RecordRoute.scss'
 
-const RecordRoute = () => {
-  const { policyId } = useParams()
-  const [displayWebCam, setDisplayWebCam] = useState(false)
-  const [showRecordStart, setShowRecordStart] = useState(true)
-  const [showRecordStop, setShowRecordStop] = useState(false)
+const mediaConstraints = {
+  video: {
+    mandatory: {
+      maxWidth: 400,
+      maxHeight: 720,
+    },
+  },
+  audio: true,
+}
 
-  const renderWebCam = () => {
-    return (<div data-testid={'webCam'}>Web Cam</div>)
+const RecordRoute = () => {
+  const {policyId} = useParams()
+
+  const webcamRef = useRef(null)
+  const videoDisplayRef = useRef({ muted: false, controls: true, autoplay: false })
+
+  const [displayWebCam, setDisplayWebCam] = useState(false)
+  const [recording, setRecording] = useState(false)
+  const [stream, setStream] = useState(null)
+  const [recordRtc, setRecordRtc] = useState(null)
+  const [videoPlayerSource, setVideoPlayerSource] = useState('')
+
+  const onUserMediaRetrievalSuccess = useCallback(streamFromMedia => {
+    const options = {
+      mimeType: 'video/webm;codecs=vp9',
+      audioBitsPerSecond: 128000,
+      videoBitsPerSecond: 128000,
+      timeSlice: 2000,
+      bitsPerSecond: 128000,
+    }
+    setStream(streamFromMedia)
+    const recordRtcLocal = RecordRTC(streamFromMedia, options)
+    setRecordRtc(recordRtcLocal)
+    recordRtcLocal.startRecording()
+  }, [setStream, setRecordRtc])
+
+  const onUserMediaRetrievalError = useCallback(error => {
+    console.warn(error)
+  }, [])
+
+  const onRecordingStopped = useCallback(audioVideoWebmUrl => {
+    setVideoPlayerSource(audioVideoWebmUrl)
+  }, [setVideoPlayerSource])
+
+  const startRecording = useCallback(() => {
+    setDisplayWebCam(true)
+    setRecording(true)
+  }, [setDisplayWebCam, setRecording])
+
+  const stopRecording = useCallback(() => {
+    recordRtc.stopRecording(onRecordingStopped)
+    stream.getAudioTracks().forEach((track) => track.stop())
+    stream.getVideoTracks().forEach((track) => track.stop())
+    setRecording(false)
+    setDisplayWebCam(false)
+  }, [onRecordingStopped, recordRtc, stream, setRecording, setDisplayWebCam])
+
+  const renderWebcam = () => {
+    return <><Webcam ref={webcamRef} width={'100%'}
+                   data-testid={'webcam'}
+                   videoConstraints={mediaConstraints}
+                   onUserMedia={onUserMediaRetrievalSuccess}
+                   onUserMediaError={onUserMediaRetrievalError} /></>
   }
 
   const renderVideoPlayer = () => {
     return (<div data-testid={'videoPlayer'}>
-      <video className="video" /*:poster="poster"*/ controls/>
+      <video ref={videoDisplayRef} className="video" controls src={videoPlayerSource}/>
     </div>)
   }
 
-  const startRecording = () => {
-    setDisplayWebCam(true)
-    setShowRecordStart(false)
-    setShowRecordStop(true)
-  }
   const renderStartRecordingOption = () => (
     <RecordingFilledAlt32 onClick={startRecording} className="btn-record-start" data-testid={'startRecording'}/>
   )
 
-  const stopRecording = () => {
-    setDisplayWebCam(false)
-    setShowRecordStart(true)
-    setShowRecordStop(false)
-  }
   const renderStopRecordingOption = () => (
     <StopFilledAlt32 onClick={stopRecording} className="btn-record-stop" data-testid={'stopRecording'}/>
   )
@@ -55,7 +102,7 @@ const RecordRoute = () => {
         <div className="bx--col r2-col">
           <div className="video-rec text-xs-center">
             <input type="hidden" name="video"/>
-            {displayWebCam && renderWebCam()}
+            {displayWebCam && renderWebcam()}
             {!displayWebCam && renderVideoPlayer()}
             <div className="video-controllers"/>
           </div>
@@ -81,8 +128,7 @@ const RecordRoute = () => {
                 <br/>10-15-2020
               </Column>
               <Column className={'col-2'}>
-                {showRecordStart && renderStartRecordingOption()}
-                {showRecordStop && renderStopRecordingOption()}
+                {recording ? renderStopRecordingOption() : renderStartRecordingOption()}
               </Column>
               <Column className={'col-3 video-length'}>
                 <div>02.00m</div>
