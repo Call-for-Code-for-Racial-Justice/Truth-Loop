7595ef05e9f7858cc896021e1d4300a59b7b1022
Ed Snodgrass <snodgrass.ed@gmail.com>
2021-10-18 20:52:49 -0500

    176 some realigning of the PublicationForm.js
    Signed-off-by: Ed Snodgrass <snodgrass.ed@gmail.com>

--- server/react-admin/src/publications/PublicationForm.js
+++ server/react-admin/src/publications/PublicationForm.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
-import {useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import Button from '@mui/material/Button'
 import Grid from '@mui/material/Grid'
 import Paper from '@mui/material/Paper'
@@ -43,43 +43,40 @@ function PublicationForm() {
   }
 
   return (
-    <Paper elevation={12} data-testid={'addPublicationForm'}>
-      <Grid container spacing={2} p={2}>
-        <Grid item>
-          <Typography component={'h2'} variant={'h6'}>Add Publication</Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <Grid container spacing={2}>
-              <Grid item lg={4} md={6} xs={12}>
-                <TextFieldInput name={'title'} control={control} label={'Title'} required/>
-              </Grid>
-              <Grid item lg={4} md={6} xs={12}>
-                <TextFieldInput name={'description'} control={control} label={'Description'} required/>
-              </Grid>
-              <Grid item lg={4} md={6} xs={12}>
-                <TextFieldInput name={'link'} control={control} label={'Link'} required/>
-              </Grid>
-              <Grid item container justifyContent={'flex-end'} spacing={2}><Grid item>
-                <Button variant="outlined" color="primary" disabled={submitting} onClick={cancel}>
-                  Cancel
-                </Button>
-              </Grid>
-                <Grid item>
-                  <Button variant="outlined" color="primary" disabled={submitting} onClick={resetForm}>
-                    Reset
-                  </Button>
-                </Grid>
-                <Grid item>
-                  <Button variant="contained" color="primary" disabled={submitting} type={'submit'}>
-                    Add
-                  </Button>
-                </Grid>
-              </Grid>
+    <Paper elevation={12} data-testid={'addPublicationForm'} sx={{p: 2}}>
+      <Typography component={'h2'} variant={'h6'} mb={2}>Add Publication</Typography>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid container spacing={2}>
+          <Grid item container spacing={1}>
+            <Grid item lg={4} md={6} xs={12}>
+              <TextFieldInput name={'title'} control={control} label={'Title'} required/>
             </Grid>
-          </form>
+            <Grid item lg={4} md={6} xs={12}>
+              <TextFieldInput name={'description'} control={control} label={'Description'} required/>
+            </Grid>
+            <Grid item lg={4} md={6} xs={12}>
+              <TextFieldInput name={'link'} control={control} label={'Link'} required/>
+            </Grid>
+          </Grid>
+          <Grid item container justifyContent={'flex-end'} spacing={1}>
+            <Grid item>
+              <Button variant="outlined" color="primary" disabled={submitting} onClick={cancel}>
+                Cancel
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button variant="outlined" color="primary" disabled={submitting} onClick={resetForm}>
+                Reset
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button variant="contained" color="primary" disabled={submitting} type={'submit'}>
+                Add
+              </Button>
+            </Grid>
+          </Grid>
         </Grid>
-      </Grid>
+      </form>
       <Snackbar
         open={!!formError}
         autoHideDuration={6000}
