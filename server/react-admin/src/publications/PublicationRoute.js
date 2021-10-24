6bc9672b6abd1a700dca0fda929c7ca3707082de
Ed Snodgrass <snodgrass.ed@gmail.com>
2021-10-24 17:45:15 -0500

    176 fixing searching issues by requiring publications, adding a loading icon, and forcing the search on first table render.
    Signed-off-by: Ed Snodgrass <snodgrass.ed@gmail.com>

--- server/react-admin/src/publications/PublicationRoute.js
+++ server/react-admin/src/publications/PublicationRoute.js
@@ -3,16 +3,20 @@ import PublicationTable from './PublicationTable'
 
 function PublicationRoute() {
   const [publications, setPublications] = useState([])
+  const [loadingPublications, setLoadingPublications] = useState(false)
+
   useEffect(() => {
     async function fetchPublications() {
+      setLoadingPublications(true)
       const publicationsResponse = await fetch('/api/v1/publications')
       return await publicationsResponse.json()
     }
     fetchPublications().then(setPublications)
+    setLoadingPublications(false)
   }, [])
   return (
     <>
-      <PublicationTable publications={publications}/>
+      <PublicationTable publications={publications} isLoading={loadingPublications}/>
     </>
   )
 }
