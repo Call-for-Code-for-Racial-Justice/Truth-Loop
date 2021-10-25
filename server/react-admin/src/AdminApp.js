c40fbf3e5890a2727ea6a89b5671f7d83aa02217
Juan Feliz <37090868+felizj17@users.noreply.github.com>
2021-10-24 20:03:54 -0400

    review fixes
    Signed-off-by: felizj17 felizj17@gmail.com

--- server/react-admin/src/AdminApp.js
+++ server/react-admin/src/AdminApp.js
@@ -14,11 +14,11 @@ function AdminApp() {
           <Switch>
             <Route name="admin-home-route" exact path={'/'} component={AdminHomeRoute} />
             <Route name="publication-route" exact path={'/publications'} component={PublicationRoute} />
-			<Route name="video-testimonial-route" exact path={'/videoTestimonials'} component={VideoTestimonialRoute} />
+			      <Route name="video-testimonial-route" exact path={'/videoTestimonials'} component={VideoTestimonialRoute} />
             <Route name="add-publication-form" exact path={'/publications/add'} component={PublicationForm}/>
             <Route name="edit-publication-form" path={'/publications/edit/:id'} component={PublicationForm}/>
-			<Route name="add-testimonial-form" exact path={'/videoTestimonials/add'} component={VideoTestimonialForm}/>
-			<Route name="edit-testimonial-form" path={'/videoTestimonial/edit/:id'} component={VideoTestimonialForm} />
+		      	<Route name="add-testimonial-form" exact path={'/videoTestimonials/add'} component={VideoTestimonialForm}/>
+			      <Route name="edit-testimonial-form" path={'/videoTestimonials/edit/:id'} component={VideoTestimonialForm} />
           </Switch>
         </div>
       </div>
