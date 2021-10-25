6351e06a0ac5c98c1ce731ed926c4bc60dc617aa
Juan Feliz <felizj17@gmail.com>
2021-10-25 18:26:53 -0400

    review edits
    Signed-off-by: Juan Feliz <felizj17@gmail.com>

--- server/react-admin/src/advocacyGroups/AdvocacyGroupsRoute.js
+++ server/react-admin/src/advocacyGroups/AdvocacyGroupsRoute.js
@@ -12,7 +12,7 @@ function AdvocacyGroupsRoute() {
   }, [])
   return (
     <>
-      <AdvocacyGroupsTable advocacyGroup={advocacyGroups}/>
+      <AdvocacyGroupsTable advocacyGroups={advocacyGroups}/>
     </>
   )
 }
