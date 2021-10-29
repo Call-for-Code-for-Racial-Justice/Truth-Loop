6f7e15fc89dba035e4a2fc9f06e365d0221d6c5f
Juan Feliz <felizj17@gmail.com>
2021-10-29 02:08:15 -0400

    added levels to react-admin
    Signed-off-by: Juan Feliz <felizj17@gmail.com>

--- server/react-admin/src/AdminApp.js
+++ server/react-admin/src/AdminApp.js
@@ -7,7 +7,8 @@ import VideoTestimonialRoute from './videoTestimonials/VideoTestimonialRoute'
 import VideoTestimonialForm from './videoTestimonials/VideoTestimonialForm'
 import AdvocacyGroupsRoute from './advocacyGroups/AdvocacyGroupsRoute'
 import AdvocacyGroupsForm from './advocacyGroups/AdvocacyGroupsForm'
-
+import LevelsRoute from './levels/LevelsRoute'
+import LevelsForm from './levels/LevelsForm'
 
 function AdminApp() {
   return (
@@ -15,16 +16,19 @@ function AdminApp() {
       <div id={'app'}>
         <div id={'main-content'} data-testid={'mainContent'}>
           <Switch>
-            <Route name="admin-home-route" exact path={'/'} component={AdminHomeRoute} />
-            <Route name="publication-route" exact path={'/publications'} component={PublicationRoute} />
-			      <Route name="video-testimonial-route" exact path={'/videoTestimonials'} component={VideoTestimonialRoute} />
-		      	<Route name="add-testimonial-form" exact path={'/videoTestimonials/add'} component={VideoTestimonialForm}/>
-			      <Route name="edit-testimonial-form" path={'/videoTestimonials/edit/:id'} component={VideoTestimonialForm} />
-			      <Route name="advocacy-group-route" exact path={'/advocacyGroups'} component={AdvocacyGroupsRoute}/>
-            <Route name="add-publication-form" exact path={'/publications/add'} component={PublicationForm}/>
-            <Route name="edit-publication-form" path={'/publications/edit/:id'} component={PublicationForm}/>
-			      <Route name="add-advocacy-group-form" exact path={'/advocacyGroups/add'} component={AdvocacyGroupsForm} />
-            <Route name="edit-advocacy-group-form" path={'/advocacyGroups/edit/:id'} component={AdvocacyGroupsForm} />
+						<Route name="admin-home-route" exact path={'/'} component={AdminHomeRoute} />
+						<Route name="publication-route" exact path={'/publications'} component={PublicationRoute} />
+						<Route name="video-testimonial-route" exact path={'/videoTestimonials'} component={VideoTestimonialRoute} />
+						<Route name="level-route" exact path={'/levels'} component={LevelsRoute} />
+						<Route name="add-testimonial-form" exact path={'/videoTestimonials/add'} component={VideoTestimonialForm}/>
+						<Route name="edit-testimonial-form" path={'/videoTestimonials/edit/:id'} component={VideoTestimonialForm} />
+						<Route name="advocacy-group-route" exact path={'/advocacyGroups'} component={AdvocacyGroupsRoute}/>
+						<Route name="add-publication-form" exact path={'/publications/add'} component={PublicationForm}/>
+						<Route name="edit-publication-form" path={'/publications/edit/:id'} component={PublicationForm}/>
+						<Route name="add-advocacy-group-form" exact path={'/advocacyGroups/add'} component={AdvocacyGroupsForm} />
+						<Route name="edit-advocacy-group-form" path={'/advocacyGroups/edit/:id'} component={AdvocacyGroupsForm} />
+						<Route name="add-level-form" exact path={'/levels/add'} component={LevelsForm}/>
+						<Route name="edit-level-form" path={'/levels/edit/:id'} component={LevelsForm}/>
           </Switch>
         </div>
       </div>
---
+++ server/react-admin/src/levels/LevelsForm.js
@@ -0,0 +1,92 @@
+import React, {useState} from 'react'
+import {useHistory, useLocation} from 'react-router-dom'
+import {useForm} from 'react-hook-form'
+import Button from '@mui/material/Button'
+import Grid from '@mui/material/Grid'
+import Paper from '@mui/material/Paper'
+import Snackbar from '@mui/material/Snackbar'
+import Typography from '@mui/material/Typography'
+import TextFieldInput from '../form/TextFieldInput'
+
+const emptyFormValues = {name: '', description: ''}
+
+function LevelsForm() {
+  const location = useLocation()
+  const existingLevels = location?.state?.levels
+  const {control, handleSubmit, reset} = useForm({defaultValues: existingLevels ? {...existingLevels} : {...emptyFormValues}})
+  const [submitting, setSubmitting] = useState(false)
+  const [formError, setFormError] = useState('')
+  const history = useHistory()
+
+  const handleCloseFormError = function () {
+    setFormError(false)
+  }
+
+  const cancel = function () {
+    history.push('/levels')
+  }
+
+  const onSubmit = async function (values) {
+    setSubmitting(true)
+    const url = existingLevels ? `/api/v1/levels/${existingLevels.id}` : '/api/v1/levels'
+    const levelsResponse = await fetch(url, {
+      method: existingLevels ? 'PUT' : 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(values)
+    })
+    if (levelsResponse.ok) {
+      history.push('/levels')
+    } else {
+      const errorText = await levelsResponse.text()
+      setSubmitting(false)
+      setFormError(errorText)
+    }
+  }
+
+  const resetForm = function () {
+    reset()
+  }
+
+  return (
+    <Paper elevation={12} data-testid={'addLevelForm'} sx={{p: 2}}>
+      <Typography component={'h2'} variant={'h6'} mb={2}>Add Level</Typography>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid container spacing={2}>
+          <Grid item container spacing={2}>
+		  <Grid item lg={4} md={6} xs={12}>
+              <TextFieldInput name={'name'} control={control} label={'Name'} required/>
+            </Grid>
+            <Grid item lg={4} md={6} xs={12}>
+              <TextFieldInput name={'description'} control={control} label={'Description'} required/>
+            </Grid>
+          </Grid>
+          <Grid item container justifyContent={'flex-end'} spacing={1}>
+            <Grid item>
+              <Button variant="outlined" color="primary" disabled={submitting} onClick={cancel}>
+                Cancel
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button variant="outlined" color="primary" disabled={submitting} onClick={resetForm}>
+                Reset
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button variant="contained" color="primary" disabled={submitting} type={'submit'}>
+                {existingLevels ? 'Update' : 'Add'}
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </form>
+      <Snackbar
+        open={!!formError}
+        autoHideDuration={6000}
+        onClose={handleCloseFormError}
+        message={formError}
+      />
+    </Paper>
+  )
+}
+
+export default LevelsForm
