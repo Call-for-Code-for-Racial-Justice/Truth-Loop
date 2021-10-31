0bf3e8c2f78197c4306f2b3ac3647bac1f1f7db9
Juan Feliz <felizj17@gmail.com>
2021-10-31 16:46:48 -0400

    used drawer from mui/material for side nav
    Signed-off-by: Juan Feliz <felizj17@gmail.com>

--- server/react-admin/src/sidemenu/SideNav.js
+++ server/react-admin/src/sidemenu/SideNav.js
@@ -1,22 +1,95 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import { styled } from '@mui/material/styles'
+import PropTypes from 'prop-types'
 import SideNavTab from './SideNavTab'
+import MuiAppBar from '@mui/material/AppBar'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import HomeIcon from '@mui/icons-material/Home'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Drawer from '@mui/material/Drawer'
+
 import './sidenav.scss'
-const SideNav = () => {
-	return(
-		<nav id="side-nav">
-		<ul id="nav-list">
-			<li><SideNavTab label="Legislative Artifacts" link="legislativeArtifacts" component="Artifacts" /></li>
-			<li><SideNavTab label="Advocacy Groups" link="advocacyGroups" component="Groups" /></li>
-			<li><SideNavTab label="Categories" link="categories" component="Categories" /></li>
-			<li><SideNavTab label="Levels" link="levels" component="Levels" /></li>
-			<li><SideNavTab label="Geographies" link="geospatialDefinitions" component="Geographies"/></li>
-			<li><SideNavTab label="Officials" link="officials" component="Officials" /></li>
-			<li><SideNavTab label="Publications" link="publications" component="Publications" /></li>
-			<li><SideNavTab label="Video Testimonials" link="videoTestimonials" component="Video Testimonial" /></li>
-			<li><SideNavTab label="Legislative Artifacts" link="legislativeArtifacts" /></li>
-			<li><SideNavTab label="API Docs" link="api-docs" /></li>
-		</ul>
-		</nav>
-	)
+
+const drawerWidth = 280
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}))
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}))
+const SideNav = ({ open, handleClose, handleOpen }) => {
+  return (
+    <nav>
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() => handleOpen()}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Truth Loop Admin UI
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="persistent" anchor="left" open={open}>
+        <DrawerHeader>
+          <a href="/" id="home-button">
+            <HomeIcon color="primary" />
+          </a>
+          <IconButton onClick={handleClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </DrawerHeader>
+        <SideNavTab label="Legislative Artifacts" link="legislativeArtifacts" />
+        <SideNavTab label="Advocacy Groups" link="advocacyGroups" />
+        <SideNavTab label="Categories" link="categories" />
+        <SideNavTab label="Levels" link="levels" />
+        <SideNavTab label="Geographies" link="geospatialDefinitions" />
+        <SideNavTab label="Officials" link="officials" />
+        <SideNavTab label="Publications" link="publications" />
+        <SideNavTab
+          label="Video Testimonials"
+          link="videoTestimonials"
+          component="Video Testimonial"
+        />
+        <SideNavTab label="Legislative Artifacts" link="legislativeArtifacts" />
+        <SideNavTab label="API Docs" link="api-docs" />
+      </Drawer>
+    </nav>
+  )
 }
+
+SideNav.propTypes = {
+  open: PropTypes.bool.isRequired,
+  handleClose: PropTypes.func.isRequired,
+  handleOpen: PropTypes.func.isRequired,
+}
+
 export default SideNav
--- server/react-admin/src/sidemenu/SideNavTab.js
+++ server/react-admin/src/sidemenu/SideNavTab.js
@@ -1,34 +1,50 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import ExpandMore from '@mui/icons-material/ExpandMore'
 import './sidenav.scss'
 
-const SideNavTab=({label, link})=>{
-	const [open, setOpen] = React.useState(false)
-	if(label=="API Docs"){
-		return(
-			<div className="menu-item">
-			<button type="button" className="menu-button" onClick={()=>setOpen(!open)}>{label}</button>
-			<div className={open?"links":"display-none"}>
-				<a className="side-nav-link" href={`/${link}`}> REST {label} </a>
-			</div>
-			</div>
-		)
-	}else{
-		return(
-			<div className="menu-item">
-				<button type="button" className="menu-button" onClick={()=>setOpen(!open)}>{label}</button>
-				<div className={open?"links":"display-none"}>
-					<a className="side-nav-link" href={`/${link}/add`}> Add {label} </a>
-					<a className="side-nav-link" href={`/${link}`}> List/Edit/Delete {label} </a>
-				</div>
-			</div>
-		)
-	}
+const SideNavTab = ({ label, link }) => {
+  const [open, setOpen] = React.useState(false)
+  if (label == 'API Docs') {
+    return (
+      <div className="menu-item">
+        <button type="button" className="menu-button" onClick={() => setOpen(!open)}>
+          {label}
+          <ExpandMore className={open ? 'expand-icon-flipped' : 'expand-icon'} />
+        </button>
+        <div className={open ? 'links' : 'display-none'}>
+          <a className="side-nav-link" href={`/${link}`}>
+            {' '}
+            REST {label}{' '}
+          </a>
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div className="menu-item">
+        <button type="button" className="menu-button" onClick={() => setOpen(!open)}>
+          {label}
+          <ExpandMore className={open ? 'expand-icon-flipped' : 'expand-icon'} />
+        </button>
+        <div className={open ? 'links' : 'display-none'}>
+          <a className="side-nav-link" href={`/${link}/add`}>
+            {' '}
+            Add {label}{' '}
+          </a>
+          <a className="side-nav-link" href={`/${link}`}>
+            {' '}
+            List/Edit/Delete {label}{' '}
+          </a>
+        </div>
+      </div>
+    )
+  }
 }
 
-SideNavTab.propTypes={
-	label:PropTypes.string.isRequired,
-	link:PropTypes.string.isRequired
+SideNavTab.propTypes = {
+  label: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
 }
 
 export default SideNavTab
