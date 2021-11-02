0aa7fec7b365574edbd0e8a19b17665a8ac6617e
Ed Snodgrass <snodgrass.ed@gmail.com>
2021-11-01 22:41:48 -0500

    202 formatting of Level* components.
    Signed-off-by: Ed Snodgrass <snodgrass.ed@gmail.com>

--- server/react-admin/src/levels/LevelsForm.js
+++ server/react-admin/src/levels/LevelsForm.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react'
-import {useHistory, useLocation} from 'react-router-dom'
-import {useForm} from 'react-hook-form'
+import React, { useState } from 'react'
+import { useHistory, useLocation } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 import Button from '@mui/material/Button'
 import Grid from '@mui/material/Grid'
 import Paper from '@mui/material/Paper'
@@ -8,12 +8,14 @@ import Snackbar from '@mui/material/Snackbar'
 import Typography from '@mui/material/Typography'
 import TextFieldInput from '../form/TextFieldInput'
 
-const emptyFormValues = {name: '', description: ''}
+const emptyFormValues = { name: '', description: '' }
 
 function LevelsForm() {
   const location = useLocation()
   const existingLevel = location?.state?.level
-  const {control, handleSubmit, reset} = useForm({defaultValues: existingLevel ? {...existingLevel} : {...emptyFormValues}})
+  const { control, handleSubmit, reset } = useForm({
+    defaultValues: existingLevel ? { ...existingLevel } : { ...emptyFormValues },
+  })
   const [submitting, setSubmitting] = useState(false)
   const [formError, setFormError] = useState('')
   const history = useHistory()
@@ -31,8 +33,8 @@ function LevelsForm() {
     const url = existingLevel ? `/api/v1/levels/${existingLevel.id}` : '/api/v1/levels'
     const levelsResponse = await fetch(url, {
       method: existingLevel ? 'PUT' : 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(values)
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(values),
     })
     if (levelsResponse.ok) {
       history.push('/levels')
@@ -48,16 +50,23 @@ function LevelsForm() {
   }
 
   return (
-    <Paper elevation={12} data-testid={'addLevelForm'} sx={{p: 2}}>
-      <Typography component={'h2'} variant={'h6'} mb={2}>Add Level</Typography>
+    <Paper elevation={12} data-testid={'addLevelForm'} sx={{ p: 2 }}>
+      <Typography component={'h2'} variant={'h6'} mb={2}>
+        Add Level
+      </Typography>
       <form onSubmit={handleSubmit(onSubmit)}>
         <Grid container spacing={2}>
           <Grid item container spacing={2}>
-						<Grid item lg={4} md={6} xs={12}>
-              <TextFieldInput name={'name'} control={control} label={'Name'} required/>
+            <Grid item lg={4} md={6} xs={12}>
+              <TextFieldInput name={'name'} control={control} label={'Name'} required />
             </Grid>
             <Grid item lg={4} md={6} xs={12}>
-              <TextFieldInput name={'description'} control={control} label={'Description'} required/>
+              <TextFieldInput
+                name={'description'}
+                control={control}
+                label={'Description'}
+                required
+              />
             </Grid>
           </Grid>
           <Grid item container justifyContent={'flex-end'} spacing={1}>
