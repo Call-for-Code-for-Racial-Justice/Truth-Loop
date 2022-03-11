0f911eef3254353f6916656fbfb177b740cfdd75
Ed Snodgrass <snodgrass.ed@gmail.com>
2022-03-10 22:14:12 -0600

    moved the swagger setup to the app.js to load that before the react app is loaded.
    Signed-off-by: Ed Snodgrass <snodgrass.ed@gmail.com>

--- server/server.js
+++ server/server.js
@@ -2,29 +2,7 @@ require('dotenv').config({ silent: true })
 const logger = require('./logger')
 const app = require('./app')
 
-const swaggerInline = require('swagger-inline')
-
-swaggerInline(['./*.js', './routes/*.js'], {
-  base: 'swaggerBase.json',
-}).then((generatedSwagger) => {
-  swaggerDocument = JSON.parse(generatedSwagger)
-  var swaggerUi = require('swagger-ui-express')
-  var options = {
-    jsonEditor: true,
-  }
-  app.use(
-    '/api-docs',
-    function (req, res, next) {
-      swaggerDocument.host = req.get('host')
-      req.swaggerDoc = swaggerDocument
-      next()
-    },
-    swaggerUi.serve,
-    swaggerUi.setup(null, options)
-  )
-
-  const PORT = process.env.PORT || 5000
-  app.listen(PORT, () => {
-    logger.logger.info('server has started on port %d', PORT)
-  })
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  logger.logger.info('server has started on port %d', PORT)
 })
